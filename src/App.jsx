45c5415637b1deb4e1a782495ddef6a079ef6bb6
Sajan Gaikwad <sajan.gaikwad21@pccoepune.com>
2024-09-19 22:13:46 +0530

    base url chnage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router basename="PortFolio">
+    <Router basename="/PortFolio/">
       <Navbar />
       <div className="pt-16">
         <Routes>
